00fcaadb0b27f76dbe2301a39485133cbab808ef
darkokostic17 <dare_93_bg@hotmail.com>
2015-08-05 14:32:15 +0200

    Baza

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿INSERT into Radnik(Id, Ime, Prezime, Plata, Datum)
+values ('11', 'Nevena', 'Dobrosavljević', '65000.76', '2015-07-10'),
+('12', 'Milica', 'Dimkić', '345890', '2015-07-19'),
+('13', 'Tijana', 'Gligorijević', '23980.45', '2015-07-03'),
+('14', 'Anja', 'Veselinović', '35000', '2015-07-04'),
+('15', 'Nikola', 'Radulović', '236000', '2015-07-09'),
+('16', 'Stefan', 'Veljković', '450000', '2015-07-15'),
+('17', 'Ivan', 'Mrđa', '600000', '2015-07-13'),
+('18', 'Emilija', 'Damnjanovic', '178900', '2015-07-10'),
+('19', 'Ognjen', 'Ašković', '156799', '2015-07-15'),
+('20', 'Marko', 'Baračkov', '214560', '2015-07-19')
