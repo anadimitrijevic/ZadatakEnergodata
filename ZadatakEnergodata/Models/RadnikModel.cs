8b8baabff3e8501b06cc2af740e0285ab5ead5b6
marijadamnjanovic <marijaamnjanovic92@gmail.com>
2015-08-06 09:47:00 +0200

    model

---
+++ ZadatakEnergodata/Models/RadnikModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZadatakEnergodata.Models
+{
+    public class RadnikModel
+    {
+        public int id { get; set; }
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public double plata { get; set; }
+        public DateTime datumPlate { get; set; }
+    }
+}
